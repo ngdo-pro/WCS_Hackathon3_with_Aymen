3a9cd51909070c5b58d30788ec262afe6b403382
ngdo-pro <ngdo27@gmail.com>
2016-12-09 11:16:52 +0100

    added redirection to locale, to have translation

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -3,9 +3,22 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class MainController extends Controller
 {
+
+    public function redirectAction(Request $request)
+    {
+        if( null != $request->getLocale()){
+            $locale = $request->getLocale();
+        }else{
+            $locale = "en";
+        }
+
+        return $this->redirectToRoute('app_homepage', array('_locale' => $locale));
+    }
+
     public function indexAction()
     {
         return $this->render('AppBundle:Main:index.html.twig');
