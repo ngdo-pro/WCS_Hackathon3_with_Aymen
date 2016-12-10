f0196064ec711d03cdd7ea4bf6f67166410ccdba
ngdo-pro <ngdo27@gmail.com>
2016-12-10 09:23:57 +0100

    installed easyAdmin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new UserBundle\UserBundle(),
             new ServiceBundle\ServiceBundle(),
+            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
