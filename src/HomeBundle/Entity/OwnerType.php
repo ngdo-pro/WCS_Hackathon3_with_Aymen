f8a055b17cd852c179188d687bc38d8d450c6e19
ngdo-pro <ngdo27@gmail.com>
2016-12-11 10:49:39 +0100

    generated HomeBundle and created 2 entities

---
+++ src/HomeBundle/Entity/OwnerType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace HomeBundle\Entity;
+
+/**
+ * OwnerType
+ */
+class OwnerType
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return OwnerType
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
+
