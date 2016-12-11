79824555e84bbc859a034590f9c82766fbb2de6c
ngdo-pro <ngdo27@gmail.com>
2016-12-11 12:04:17 +0100

    changed relationship between tables

---
+++ src/HomeBundle/DataFixtures/ORM/LoadOwnerTypeData.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicolas
+ * Date: 11/12/16
+ * Time: 11:59
+ */
+
+namespace HomeBundle\DataFixtures\ORM;
+
+
+class LoadOwnerTypeData
+{
+
+}
---
+++ src/HomeBundle/DataFixtures/ORM/LoadRentData.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicolas
+ * Date: 11/12/16
+ * Time: 11:58
+ */
+
+namespace HomeBundle\DataFixtures\ORM;
+
+
+class LoadRentData
+{
+
+}
